077f6c228b8b2617c1ee7ebd5f907610fa7aa470
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2021-02-13 00:18:30 +0800

    updated to stay away from .bashrc

--- bin/cloud-ci-pre-init.sh
+++ bin/cloud-ci-pre-init.sh
@@ -2,3 +2,5 @@
 
 git clone --bare --branch main --depth=1 --no-tags https://github.com/yogendra/cloud-ci.git $HOME/.cloud-ci
 git --git-dir=$HOME/.cloud-ci/ --work-tree=$HOME checkout --force
+
+echo "[[ -f \$HOME/.bashrc.cloud-ci ]] &&  source \$HOME/.bashrc.cloud-ci" >> $HOME/.bashrc
