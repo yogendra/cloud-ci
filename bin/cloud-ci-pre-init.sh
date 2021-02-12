d9dec93fcc59e8e65b8b8c85569c10e574a2e1d9
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2021-02-12 23:48:31 +0800

    update pre-init

--- bin/cloud-ci-pre-init.sh
+++ bin/cloud-ci-pre-init.sh
@@ -2,4 +2,3 @@
 
 git clone --bare --branch main --depth=1 --no-tags https://github.com/yogendra/cloud-ci.git $HOME/.cloud-ci
 git --git-dir=$HOME/.cloud-ci/ --work-tree=$HOME checkout --force
-
--- bin/cloud-ci-update.sh
+++ bin/cloud-ci-update.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
 git --git-dir=$HOME/.cloud-ci/ --work-tree=$HOME pull --rebase
+
